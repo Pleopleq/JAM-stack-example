d08d861b45999b3bb213f14f458963d9a7926c33
Felix Anducho <felix.anducho74@gmail.com>
2020-06-04 15:52:22 -0500

    Add files

---
+++ sections/basic/main.js
@@ -0,0 +1,25 @@
+const listRepos = async username => {
+    const repos = await fetch(
+        `https://api.github.com/users/${username}/repos?type=owner&sort=updated`
+    )
+    .then(res => res.json())
+    .catch(error => console.log(error))
+
+    const markup = repos
+    .map(
+        repo => 
+        `
+        <li>
+            <a href="${repo.html_url}"> ${repo.name} </a>
+        </li>
+        `
+    )
+    .join('')
+    
+    const content = document.querySelector('#content')
+
+    content.innerHTML = `<ul>${markup}</ul>`
+    
+}
+
+listRepos('Pleopleq')
